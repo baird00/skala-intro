bbb61017eef15f7879bdcc884edb80a54f3c1db7
baird00 <ssy2310@gmail.com>
2026-07-14 16:54:26 +0900

    ignore수정

--- echo.py
+++ echo.py
@@ -1,6 +1,9 @@
 def main():
-    user_input = input("문장을 입력하세요: ")
-    print("입력하신 문자은:", user_input)
+    while True:
+        user_input = input("문장을 입력하세요 !quit입력시 종료: ")
+        if user_input == "!quit":
+            break
+        print("입력하신 문자은:", user_input)
 
 if __name__ == "__main__":
     main()
